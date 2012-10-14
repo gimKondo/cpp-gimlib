7cd6f536e3d34a26d706ebfc07824093c37d712d
gimKondo <dkondo0731@msd.biglobe.ne.jp>
2012-10-15 01:57:37 +0900

    add code sample in comment for mixin helper

--- gimlib/include/mixin.h
+++ gimlib/include/mixin.h
@@ -19,6 +19,36 @@ namespace GimLib {
 		You can mix-in features, if you extend from this class
 		that receiving feature class as type parameter.
 		Features are passed as TypeList.
+	@code
+		// feature class 1
+		template <typename THIS_TYPE> struct Sum
+		{
+			int sum()
+			{
+				THIS_TYPE* this_ = static_cast<THIS_TYPE*>(this);
+				return this_->num1 + this_->num2;
+			}
+		};
+		// feature class 2
+		template <typename THIS_TYPE> struct Sub
+		{
+			int sub()
+			{
+				THIS_TYPE* this_ = static_cast<THIS_TYPE*>(this);
+				return this_->num1 - this_->num2;
+			}
+		};
+
+		// mixed-in class
+		struct MixedinClass : public MixinBase<MixedinClass, TYPELIST_2(Sum<MixedinClass>, Sub<MixedinClass>)>
+		{
+			MixedinClass(int n1, int n2)
+				: num1(n1), num2(n2)
+			{}
+			int num1;
+			int num2;
+		};
+	@endcode
 ************************************************************************
 	@note
 		now writing...

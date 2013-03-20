fe7d87f2ac929754a0b030e755a5f19d8012c401
gim_kondo <dkondo0731@msd.biglobe.ne.jp>
2013-03-20 18:15:22 +0900

    add method for adding constency

---
+++ gimlib/include/TypeCast.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDED_GIMLIB_TYPECAST
+#define INCLUDED_GIMLIB_TYPECAST
+
+namespace GimLib {
+
+//! const‚ð•t‰Á‚·‚é
+template<typename T>
+const T& add_const(const T& x) { return x; }
+
+
+} // end namespace GimLib
+
+#endif

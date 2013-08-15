88729f792e8bb5a4828644478eb27507ba320914
gim_kondo <dkondo0731@msd.biglobe.ne.jp>
2013-08-15 23:28:15 +0900

    modify a bug on unit test of extend method

--- unittest/Test/TestExtendMethod.cpp
+++ unittest/Test/TestExtendMethod.cpp
@@ -70,7 +70,7 @@ TEST(ExntedMethodTest, VectorMethod)
 	auto widArr = (GimLib::make_vector<Widget>()
 		, Widget( 39, "sample", 20.0 )
 		, Widget( 39, "sample", 20.0 )
-		, Widget( 39, "sample", 20.0 ) );
+		, Widget( 39, "sample", 20.0 ) )();
 	EXPECT_DOUBLE_EQ(60.0, widArr->*WeightSum());
 }
 	
